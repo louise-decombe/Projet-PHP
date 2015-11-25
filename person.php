c7ec68f9de297751b216f4dc61399ca55350b7d5
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2015-11-25 22:33:55 +0100

    Création de classe personne

---
+++ person.php
@@ -0,0 +1,29 @@
+<?php
+
+class Person
+{
+    private $name;
+    private $firstname;
+    private $company;
+    private $adress;
+    private $cp;
+    private $city;
+    private $country;
+    private $phone;
+    private $website;
+    private $situation;
+
+    public function __construct($name, $firstname, $company, $adress, $cp, $city, $country, $phone, $website, $situation)
+    {
+        $this->name = $name;
+        $this->firstname = $firstname;
+        $this->company = $company;
+        $this->adress = $adress;
+        $this->cp = $cp;
+        $this->city = $city;
+        $this->country = $country;
+        $this->phone = $phone;
+        $this->website = $website;
+        $this->situation = $situation;
+    }
+}
