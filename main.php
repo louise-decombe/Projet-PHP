82fccdaf39341c7a9f0682fdcb117f0952d478c0
Anne-Sophie Saint-Omer <anne.sophie.saint.omer28@gmail.com>
2015-11-26 21:34:01 +0100

    Creation main

--- main.php
+++ main.php
@@ -1,6 +1,9 @@
 <?php
 
-	$db = new Database();
+	include("person.php");
+	include("database.php");
 
-
+	$contactsDatabase = new Database();
+	$p = new Person($_POST['name'], $_POST['firstname'], $_POST['company'], $_POST['adress'], $_POST['cp'], $_POST['city'], $_POST['country'], $_POST['phone'], $_POST['email'], $_POST['website'], $_POST['situation']);
+	$contactsDatabase->addPerson($p);
 ?>
